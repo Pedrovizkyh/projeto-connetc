a425abc9c6728c88207572311af08714e81802db
Pedro Bastos <henriquepedro.2001@gmail.com>
2026-02-05 21:49:23 -0300

    feat: estruturando o header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import s from "./header.module.scss"
+import { Link } from "react-router-dom"
+import logo from "../../assets/logo.png"
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+          <header>
+            <Link to={"/"}>
+              <img className={s.imgLogo} src={logo} alt="Imagem de logo do projeto connect sendo representada por uma mão segurando um coração, simbolizando apoio" />
+            </Link>
+
+            <nav className={s.nav}>
+            <Link className={s.Link} to={"/doacao"}>Doação</Link>
+            <Link className={s.Link} to={"/voluntariado"}>Voluntariado</Link>
+            <Link className={s.Link} to={"/mentoria"}>Mentoria</Link>
+            <Link className={s.Link} to={"/eventosEP"}>Eventos</Link>
+            </nav>
+          </header>
+    </>
   )
 }
