0cadc4686a699c14fafdf46082378628396c784d
Pedro Bastos <henriquepedro.2001@gmail.com>
2026-02-10 19:03:24 -0300

    feat: montando a pagina inicial

--- src/pages/paginaInicial/PaginaInicial.jsx
+++ src/pages/paginaInicial/PaginaInicial.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import s from './paginaInicial.module.scss'
+import Img_maos from '../../assets/imagens/img_maos.png'
+import Seta from '../../assets/icones/seta.png'
 
 export default function PaginaInicial() {
   return (
     <>
+    <section>
+      <div className={s.principal}>
+        <h1>Projetos Sociais que transformam</h1>
+        <p className={s.texto}>Conectamos sua empresa com projetos sociais impactantes. Juntos, criamos mudanças reais na comunidade.</p>
+        <button>Cadastrar empresa <img src={Seta} alt="" /></button>
+        
+        <div className={s.articles}>
+          <article>
+            <h2>500+</h2>
+            <p>Empresas Voluntárias</p>
+          </article>
+          <article>
+            <h2>1.2K+</h2>
+            <p>Projetos Realizados</p>
+          </article>
+          <article>
+            <h2>50K+</h2>
+            <p>Vidas Impactadas </p>
+          </article>
+        </div>
+      </div>
+
+      <img src={Img_maos} />
+    </section>
     </>
   )
 }
