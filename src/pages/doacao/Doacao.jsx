b7cafee0356ed3e81540ed2bc5731b5e2397c932
Pedro Bastos <henriquepedro.2001@gmail.com>
2026-02-10 23:41:41 -0300

    feat: fazendo a página de Doação

--- src/pages/doacao/Doacao.jsx
+++ src/pages/doacao/Doacao.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import s from './doacao.module.scss'
+import Caixas from '../../assets/imagens/doacao/carregando-caixas.jpg'
+import Livros from '../../assets/imagens/doacao/livros.jpg'
+import Computador from '../../assets/imagens/doacao/computador-aberto.jpg'
 
 export default function Doacao() {
   return (
     <>
+      <main>
+        <section>
+          <h2>Doação</h2>
+        <div className={s.articles}>
+          <article>
+            <img src={Caixas} alt="" />
+            <h3>Instituto grande familia</h3>
+            <p>Contribua com alimentos não perecíveis e ajude famílias em situação de vulnerabilidade.</p>
+            <button>Quero Doar</button>
+          </article>
+          <div className={s.linha}></div>
+          <article>
+            <img src={Livros} alt="" />
+            <h3>Projeto Futuro na Escola</h3>
+            <p>Doe livros, cadernos, lápis, mochilas e canetas para ajudar jovens a continuarem seus estudos com mais estrutura.</p>
+            <button>Quero Doar</button>
+          </article>
+          <div className={s.linha}></div>
+          <article>
+            <img src={Computador} alt="" />
+            <h3>Instituto Conecta Jovem</h3>
+            <p>Doe computadores usados, tablets ou celulares em bom estado para permitir que jovens tenham acesso ao mundo digital e novas oportunidades.</p>
+            <button>Quero Doar</button>
+          </article>
+        </div>
+        </section>
+      </main>
+      
+
     </>
   )
 }
